e896f41c34fda1dd7243c020b0d4c29385414f0a
= <slyngbae@cslvm75.csc.calpoly.edu>
2012-03-09 21:38:29 -0800

    Fixes...

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,5 +1,12 @@
 $(document).ready(function (){
 
+	// Smooth Scrolling
+
+	$(".scroll").click(function(event){
+		event.preventDefault();
+		$('html,body').animate({scrollTop:$(this.hash).offset().top-50}, 500);
+	});
+
 	/********\
 	| Node 1 |
 	\********/
